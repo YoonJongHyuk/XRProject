1d2b31ac51fcce09e2046fbb3a1c4ba8df2b2ff2
JongHyuk Yoon <mana951@naver.com>
2023-08-29 12:09:14 +0900

    8월 28일때 한거
    늦은 commit

--- Assets/Tutorial/Scripts/Mob/Mob.cs
+++ Assets/Tutorial/Scripts/Mob/Mob.cs
@@ -5,6 +5,19 @@ using UnityEngine.AI;
 
 public class Mob : MonoBehaviour
 {
+    public float hueMin = 0f;
+    public float hueMax = 1f;
+    public float saturationMin = 0.7f;
+    public float saturationMax = 1f;
+    public float valueMin = 0.7f;
+    public float valueMax = 1f;
+
+    public float arrangeRange = 0.5f;
+
+    public float emissionIntensity = 5f;
+
+    public ParticleSystem enviromentParticle;
+
     private NavMeshAgent agent;
 
     private void Awake()
@@ -16,5 +29,18 @@ public class Mob : MonoBehaviour
     {
         agent.SetDestination(new Vector3(0f, 2f, 1f));
         agent.speed *= Random.Range(0.8f, 1.5f);
+
+        RandomColor();
+    }
+
+    private void RandomColor()
+    {
+        var color = Random.ColorHSV(hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax);
+
+        var main = enviromentParticle.main;
+        main.startColor = new ParticleSystem.MinMaxGradient(color, color * Random.Range(1f - arrangeRange, 1f + arrangeRange));
+
+        var renderer = enviromentParticle.GetComponent<ParticleSystemRenderer>();
+        renderer.material.SetColor("_EmissionColor", color * emissionIntensity);
     }
 }
