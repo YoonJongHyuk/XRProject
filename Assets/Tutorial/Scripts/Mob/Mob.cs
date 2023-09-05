5c40ded511101f26335f66ae99f9f8337149248f
JongHyuk Yoon <mana951@naver.com>
2023-09-05 13:15:20 +0900

    강의 갱신
    총에서 레이저 발사 까지 완료

--- Assets/Tutorial/Scripts/Mob/Mob.cs
+++ Assets/Tutorial/Scripts/Mob/Mob.cs
@@ -1,46 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Mob : MonoBehaviour
 {
-    public float hueMin = 0f;
-    public float hueMax = 1f;
-    public float saturationMin = 0.7f;
-    public float saturationMax = 1f;
-    public float valueMin = 0.7f;
-    public float valueMax = 1f;
+    public float destroyDelay = 1f;
 
-    public float arrangeRange = 0.5f;
+    public UnityEvent OnCreated;
+    public UnityEvent OnDestroyed;
 
-    public float emissionIntensity = 5f;
-
-    public ParticleSystem enviromentParticle;
-
-    private NavMeshAgent agent;
-
-    private void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
+    private bool isDestroyed = false;
 
     private void Start()
     {
-        agent.SetDestination(new Vector3(0f, 2f, 1f));
-        agent.speed *= Random.Range(0.8f, 1.5f);
+        Invoke(nameof(Destroy), 3f);
 
-        RandomColor();
+        OnCreated?.Invoke();
     }
 
-    private void RandomColor()
+
+    public void Destroy()
     {
-        var color = Random.ColorHSV(hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax);
+        if (isDestroyed)
+            return;
+        isDestroyed = true;
 
-        var main = enviromentParticle.main;
-        main.startColor = new ParticleSystem.MinMaxGradient(color, color * Random.Range(1f - arrangeRange, 1f + arrangeRange));
+        Destroy(gameObject, destroyDelay);
 
-        var renderer = enviromentParticle.GetComponent<ParticleSystemRenderer>();
-        renderer.material.SetColor("_EmissionColor", color * emissionIntensity);
+        OnDestroyed?.Invoke();
     }
 }
